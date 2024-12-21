b193525fb2fd4cd55ec086c0e0b7a0d7206cb23f
chen <yurenchen@yeah.net>
2024-12-21 09:41:00 +0800

    U webhook.sh: change wdir

--- webhook.sh
+++ webhook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cd ~/cloud2
-nohup /share/upload/webhook -debug -verbose -port 7001 -hooks webhook.json -template &> webhook.out &
+dir=`dirname $(readlink -f $BASH_SOURCE)`
+cd $dir
+
+nohup ./webhook -debug -verbose -port 7001 -hooks webhook.json -template &> webhook.out &
+
 
