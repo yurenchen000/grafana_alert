8182151f40970269769c3a2143a27811dc64d223
chen <yurenchen@yeah.net>
2024-12-30 16:13:00 +0800

    U grafana_alert.sh: improve push script

--- grafana_alert.sh
+++ grafana_alert.sh
@@ -20,6 +20,10 @@ msg="$2"
 name="$3"
 txt="`cat $fname`"
 
+msg="${msg//localhost:/jk4:}"
+
+. ./gf_notify.sh
+
 # call_cmd $mac 'echo dn_ok_`hwinfo get mac`'
 push_old=0
 push_cnt=0
@@ -27,26 +31,9 @@ push_delay=30
 push_msg(){
 	local tit="$1"
 	local msg="$2"
-	local tok="${3:-PUSH_PLUS_TOKEN}"
-	push_now=`date +%s`
-	if [ $((push_now-push_old)) -le $push_delay ]; then
-		echo -e "\e[31m push_msg too fast ($push_cnt recent), wait $push_delay sec!\e[0m"
-		return
-	fi
 
-	push_cnt=$((push_cnt+1))		#push_cnt
-	if [ $((push_now-push_old)) -ge 3600 ]; then
-		push_cnt=0		#1hour clear
-	fi
-
-	if [ $push_cnt -ge 5 ]; then	#push_delay
-		push_delay=600	#10min
-	else
-		push_delay=30	#.5min
-	fi
-
-	push_old=$push_now
-	curl -s http://pushplus.hxtrip.com/send -d "token=$tok" -d "title=$tit" -d "content=$msg" #-H 'Content-Type: text/markdown'
+	# curl -s http://pushplus.hxtrip.com/send -d "token=$tok" -d "title=$tit" -d "content=$msg" #-H 'Content-Type: text/markdown'
+  push_notify "$tit" "$msg"
 }
 
 
@@ -54,11 +41,12 @@ qqq='```'
 body="## $name
 ----<br>
 $msg
-----<br>
-$qqq
-$txt
-$qqq
 ----"
+
+# ----<br>
+# $qqq
+# $txt
+# $qqq
 
 echo "=== `date -Is`: $title -- $body" >> grafana_alert.out
 
